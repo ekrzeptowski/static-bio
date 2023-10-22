f64c76ef9574e74eb20d260fbcb056873d4a1b82
ekrzeptowski <kontakt@ekrzeptowski.pl>
2023-10-22 22:02:19 +0200

    Add simple-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import {defineConfig} from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,12 @@ export default defineConfig({
       external: ["svgo"],
     },
   },
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        "simple-icons": ["*"],
+      },
+    }),
+  ],
 });
